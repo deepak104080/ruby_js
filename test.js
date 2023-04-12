8c97a8e188010366257aa6475b3cc3c1b07686bd
deepak104080 <deepak104080@gmail.com>
2023-04-12 23:14:41 +0530

    12th April 2023 - Variables, Loops, Conditionals, Functions

--- test.js
+++ test.js
@@ -1,1 +1,34 @@
 console.log('Hello world in external js file.');
+
+
+let num = 10;
+
+let str = 'string';
+
+let arr = [1,2,3,4,5];
+
+let sym = Symbol('abc');
+
+
+console.log('Number is - ', num, typeof(num));
+console.log('String is - ', str, typeof(str));
+console.log('Array is - ', arr, typeof(arr));
+console.log('Symbol is - ', sym, typeof(sym));
+
+
+
+let a
+console.log(a, typeof(a));
+
+let b = 0;
+console.log(b, typeof(b));
+
+let c = null;
+console.log(c, typeof(c));
+
+let e = 5 / 'abc';
+console.log(e, typeof(e));
+
+console.log(d)
+
+
