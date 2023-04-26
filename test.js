9e692372dd2c0dd93c3f7099acd017ea96d237b7
deepak104080 <deepak104080@gmail.com>
2023-04-27 01:17:49 +0530

    26th April 2023 - DOM

--- test.js
+++ test.js
@@ -1,35 +1,65 @@
-console.log(document)
-console.log('Hello world in external js file.');
+window.onload = function() {
+    console.log(document.getElementById('notice').innerHTML);
+    console.log(document.getElementById('notice').innerText);
+    console.log(document.getElementById('notice').textContent);
+
+    // document.getElementsByClassName('uui-heading-xlarge')[0].textContent = 'New Text';
 
 
-let num = 10;
-
-let str = 'string';
-
-let arr = [1,2,3,4,5];
-
-let sym = Symbol('abc');
+    document.getElementById('engine').setAttribute('width', '400')
+    document.getElementById('engine').getAttribute('src')
 
 
-console.log('Number is - ', num, typeof(num));
-console.log('String is - ', str, typeof(str));
-console.log('Array is - ', arr, typeof(arr));
-console.log('Symbol is - ', sym, typeof(sym));
+    document.getElementById('id1').style.color = 'red'
+    document.getElementById('id1').style.display = 'none'
+    document.getElementById('id1').style.display = 'block'
+
+    //find all the methods to hide show any element
+
+
+    document.getElementById('id2').classList.add('hahahahaha');
+    document.getElementById('id2').classList.remove('hahahahaha');
+
+    document.getElementById('engine').hasAttribute('height')
+    document.getElementById('engine').hasAttribute('width')
+    document.getElementById('engine').removeAttribute('width')
+}
 
 
 
-let a
-console.log(a, typeof(a));
 
-let b = 0;
-console.log(b, typeof(b));
+// console.log(document)
+// console.log('Hello world in external js file.');
 
-let c = null;
-console.log(c, typeof(c));
 
-let e = 5 / 'abc';
-console.log(e, typeof(e));
+// let num = 10;
 
-console.log(d)
+// let str = 'string';
+
+// let arr = [1,2,3,4,5];
+
+// let sym = Symbol('abc');
+
+
+// console.log('Number is - ', num, typeof(num));
+// console.log('String is - ', str, typeof(str));
+// console.log('Array is - ', arr, typeof(arr));
+// console.log('Symbol is - ', sym, typeof(sym));
+
+
+
+// let a
+// console.log(a, typeof(a));
+
+// let b = 0;
+// console.log(b, typeof(b));
+
+// let c = null;
+// console.log(c, typeof(c));
+
+// let e = 5 / 'abc';
+// console.log(e, typeof(e));
+
+// console.log(d)
 
 
